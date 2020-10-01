b8275af57ee53261e73054e04c55e531fd2f4842
Stefan Baebler <stefan.baebler@gmail.com>
2020-10-01 15:43:34 +0200

    Scraping franchises and fuel type

--- update.sh
+++ update.sh
@@ -4,6 +4,12 @@
 # https://ec.haxx.se/cmdline/cmdline-globbing
 curl "https://goriva.si/api/v1/search/?format=json&franchise=&name=&o=&page=[1-22]&position=&radius=" -o "data/search_page_#1.json"
 
+# https://goriva.si/api/v1/fuel/
+curl "https://goriva.si/api/v1/fuel/?format=json" -o "data/fuel.json"
+
+# https://goriva.si/api/v1/franchise/
+curl "https://goriva.si/api/v1/franchise/?format=json" -o "data/franchise.json"
+
 for f in data/*.json
 do
     echo "Prettyfying $f"
